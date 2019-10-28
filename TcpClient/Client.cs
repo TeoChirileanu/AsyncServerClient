7218fa7bcbcfa581acd975b24f75581d104f0777
Teodor <teodorchirileanu@gmail.com>
2019-10-28 21:35:26 +0100

    added client and some other files :-??

---
+++ TcpClient/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using NetworkClient.Properties;
+
+namespace NetworkClient
+{
+    public static class Client
+    {
+        private const int Port = 1337;
+        private static readonly IPAddress IpAddress = IPAddress.Parse("127.0.0.1");
+
+        public static async Task Main()
+        {
+            try
+            {
+                using var client = new TcpClient();
+                await Run(client);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static async Task Run(TcpClient client)
+        {
+            Console.WriteLine(Resources.Connecting);
+            await client.ConnectAsync(IpAddress, Port);
+            Console.WriteLine(Resources.Connected);
+
+            await using var stream = client.GetStream();
+            var rawData = new Memory<byte>(new byte[1024 * 1000 * 500]);
+            var bytesRead = await stream.ReadAsync(rawData);
+            Console.WriteLine(Resources.ReadData, bytesRead);
+        }
+    }
+}
